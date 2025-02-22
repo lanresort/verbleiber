24e96fe05809f938a7c5ff06344b3b05ba43452a
Jochen Kupperschmidt <homework@nwsnet.de>
2025-02-22 12:12:34 +0100

    Use ureq agent
    Eases transition to ureq v3.x.

--- src/api.rs
+++ src/api.rs
@@ -6,14 +6,14 @@
 use anyhow::{anyhow, Result};
 use serde::Deserialize;
 use std::time::Duration;
-use ureq::Error;
+use ureq::{Agent, AgentBuilder, Error};
 
 use crate::config::ApiConfig;
 
 pub(crate) struct ApiClient {
     pub base_url: String,
     pub auth_token: String,
-    pub timeout: Duration,
+    agent: Agent,
 }
 
 #[derive(Debug, Deserialize)]
@@ -34,16 +34,16 @@ impl ApiClient {
         Self {
             base_url: config.base_url.to_owned(),
             auth_token: config.auth_token.to_owned(),
-            timeout: Duration::from_secs(config.timeout_in_seconds),
+            agent: AgentBuilder::new()
+                .timeout(Duration::from_secs(config.timeout_in_seconds))
+                .build(),
         }
     }
 
     pub(crate) fn get_tag_details(&self, tag: &str) -> Result<Option<TagDetails>> {
         let url = format!("{}/tags/{}", &self.base_url, tag);
         let authz_value = format!("Bearer {}", &self.auth_token);
-        let request = ureq::get(&url)
-            .timeout(self.timeout)
-            .set("Authorization", &authz_value);
+        let request = self.agent.get(&url).set("Authorization", &authz_value);
 
         match request.call() {
             Ok(response) => response
@@ -64,8 +64,9 @@ impl ApiClient {
         let url = format!("{}/statuses", self.base_url);
         let authz_value = format!("Bearer {}", self.auth_token);
 
-        match ureq::post(&url)
-            .timeout(self.timeout)
+        match self
+            .agent
+            .post(&url)
             .set("Authorization", &authz_value)
             .send_json(ureq::json!({
                 "user_id": user_id,
