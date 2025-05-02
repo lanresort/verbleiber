40e89971443b5cf436f16834b0c02c4f14493251
Jochen Kupperschmidt <homework@nwsnet.de>
2025-05-02 09:09:02 +0200

    Build binary with Rust 1.86.0 instead of 1.85.0

--- build-linux-binary.sh
+++ build-linux-binary.sh
@@ -2,6 +2,6 @@
 
 # Build binary
 
-IMAGE=ghcr.io/homeworkprod/rust-build-containers:rust-1.85.0-alsa
+IMAGE=ghcr.io/homeworkprod/rust-build-containers:rust-1.86.0-alsa
 
 docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp $IMAGE cargo build --release
