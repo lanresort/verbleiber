339b30abed0dd3ce61ab9a7411925f28565ec270
Jochen Kupperschmidt <homework@nwsnet.de>
2025-05-14 01:04:35 +0200

    Inline `Authorization` header value variables

--- src/api.rs
+++ src/api.rs
@@ -58,12 +58,11 @@ impl ApiClient {
 
     pub(crate) fn get_tag_details(&self, tag: &str) -> Result<Option<TagDetails>> {
         let url = format!("{}/tags/{}", &self.base_url, tag);
-        let authz_value = format!("Bearer {}", &self.api_token);
 
         match self
             .agent
             .get(&url)
-            .header("Authorization", &authz_value)
+            .header("Authorization", format!("Bearer {}", self.api_token))
             .call()
         {
             Ok(mut response) => response
@@ -78,12 +77,11 @@ impl ApiClient {
 
     pub(crate) fn sign_on(&self) -> Result<()> {
         let url = format!("{}/client/sign_on", self.base_url);
-        let authz_value = format!("Bearer {}", self.api_token);
 
         match self
             .agent
             .post(&url)
-            .header("Authorization", &authz_value)
+            .header("Authorization", format!("Bearer {}", self.api_token))
             .header("x-whereabouts-client-token", &self.client_token)
             .send_empty()
         {
@@ -95,12 +93,11 @@ impl ApiClient {
 
     pub(crate) fn sign_off(&self) -> Result<()> {
         let url = format!("{}/client/sign_off", self.base_url);
-        let authz_value = format!("Bearer {}", self.api_token);
 
         match self
             .agent
             .post(&url)
-            .header("Authorization", &authz_value)
+            .header("Authorization", format!("Bearer {}", self.api_token))
             .header("x-whereabouts-client-token", &self.client_token)
             .send_empty()
         {
@@ -117,12 +114,11 @@ impl ApiClient {
         whereabouts_name: &str,
     ) -> Result<()> {
         let url = format!("{}/statuses", self.base_url);
-        let authz_value = format!("Bearer {}", self.api_token);
 
         match self
             .agent
             .post(&url)
-            .header("Authorization", &authz_value)
+            .header("Authorization", format!("Bearer {}", self.api_token))
             .header("x-whereabouts-client-token", &self.client_token)
             .send_json(StatusUpdate {
                 user_id: user_id.to_string(),
