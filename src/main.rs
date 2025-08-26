ad503b84801eaa7438e8699fc069711a1f4b5da6
Jochen Kupperschmidt <homework@nwsnet.de>
2025-08-27 00:46:07 +0200

    Move tag read handling into `Client`

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod model;
 mod random;
 mod tagreader;
 
-use crate::api::{ApiClient, TagDetails};
+use crate::api::ApiClient;
 use crate::audio::AudioPlayer;
 use crate::buttons::Button;
 use crate::config::ApiConfig;
@@ -66,41 +66,7 @@ fn main() -> Result<()> {
         match msg {
             Event::TagRead { tag } => {
                 log::debug!("Tag read: {tag}");
-
-                log::debug!("Requesting details for tag {} ...", tag);
-                current_user_id = match client.get_tag_details(&tag) {
-                    Ok(details) => match details {
-                        Some(details) => {
-                            log::debug!(
-                                "User for tag {}: {} (ID: {})",
-                                details.identifier,
-                                details.user.screen_name.unwrap_or("<nameless>".to_string()),
-                                details.user.id
-                            );
-                            let user_id = details.user.id;
-
-                            if let Some(name) = details.sound_name {
-                                client.play_sound(&name)?;
-                            }
-
-                            log::debug!("Awaiting whereabouts for user {user_id} ...");
-
-                            Some(user_id.to_string())
-                        }
-                        None => {
-                            log::info!("Unknown user tag: {tag}");
-                            client.play_sound("unknown_user_tag")?;
-
-                            None
-                        }
-                    },
-                    Err(e) => {
-                        log::warn!("Requesting tag details failed.\n{e}");
-                        client.play_sound("oh-nein-netzwerkfehler")?;
-
-                        None
-                    }
-                };
+                current_user_id = client.handle_tag_read(&tag)?;
             }
             Event::ButtonPressed { button } => {
                 log::debug!("Button pressed: {:?}", button);
@@ -199,8 +165,41 @@ impl Client {
         Ok(())
     }
 
-    fn get_tag_details(&self, tag: &str) -> Result<Option<TagDetails>> {
-        self.api_client.get_tag_details(tag)
+    fn handle_tag_read(&self, tag: &str) -> Result<Option<UserId>> {
+        log::debug!("Requesting details for tag {} ...", tag);
+        match self.api_client.get_tag_details(tag) {
+            Ok(details) => match details {
+                Some(details) => {
+                    log::debug!(
+                        "User for tag {}: {} (ID: {})",
+                        details.identifier,
+                        details.user.screen_name.unwrap_or("<nameless>".to_string()),
+                        details.user.id
+                    );
+                    let user_id = details.user.id;
+
+                    if let Some(name) = details.sound_name {
+                        self.play_sound(&name)?;
+                    }
+
+                    log::debug!("Awaiting whereabouts for user {user_id} ...");
+
+                    Ok(Some(user_id.to_string()))
+                }
+                None => {
+                    log::info!("Unknown user tag: {tag}");
+                    self.play_sound("unknown_user_tag")?;
+
+                    Ok(None)
+                }
+            },
+            Err(e) => {
+                log::warn!("Requesting tag details failed.\n{e}");
+                self.play_sound("oh-nein-netzwerkfehler")?;
+
+                Ok(None)
+            }
+        }
     }
 
     fn update_status(&self, user_id: &str, whereabouts_name: &str) -> Result<()> {
