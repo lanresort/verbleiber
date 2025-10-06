026e08eed569305196eec00003e5f568a851d4f2
Jochen Kupperschmidt <homework@nwsnet.de>
2025-10-06 09:20:48 +0200

    Extract `Client` into separate module

---
+++ src/client.rs
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2022-2025 Jochen Kupperschmidt
+ * License: MIT
+ */
+
+use anyhow::Result;
+use std::path::PathBuf;
+
+use crate::api::ApiClient;
+use crate::audio::AudioPlayer;
+use crate::buttons::Button;
+use crate::config::{ApiConfig, PartyConfig};
+use crate::model::{PartyId, UserId};
+use crate::random::Random;
+
+pub struct Client {
+    audio_player: AudioPlayer,
+    random: Random,
+    api_client: ApiClient,
+}
+
+impl Client {
+    pub fn new(sounds_path: PathBuf, api_config: &ApiConfig, party_id: PartyId) -> Result<Self> {
+        Ok(Self {
+            audio_player: AudioPlayer::new(sounds_path)?,
+            random: Random::new(),
+            api_client: ApiClient::new(api_config, party_id),
+        })
+    }
+
+    pub fn sign_on(&self) -> Result<()> {
+        log::info!("Signing on ...");
+        match self.api_client.sign_on() {
+            Ok(()) => log::info!("Signed on."),
+            Err(e) => {
+                log::warn!("Signing on failed.\n{e}");
+                self.play_sound("oh-nein-netzwerkfehler")?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn sign_off(&self) -> Result<()> {
+        log::info!("Signing off ...");
+        match self.api_client.sign_off() {
+            Ok(()) => log::info!("Signed off."),
+            Err(e) => {
+                log::warn!("Signing off failed.\n{e}");
+                self.play_sound("oh-nein-netzwerkfehler")?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn handle_tag_read(&self, tag: &str) -> Result<Option<UserId>> {
+        log::debug!("Requesting details for tag {} ...", tag);
+        match self.api_client.get_tag_details(tag) {
+            Ok(details) => match details {
+                Some(details) => {
+                    log::debug!(
+                        "User for tag {}: {} (ID: {})",
+                        details.identifier,
+                        details.user.screen_name.unwrap_or("<nameless>".to_string()),
+                        details.user.id
+                    );
+                    let user_id = details.user.id;
+
+                    if let Some(name) = details.sound_name {
+                        self.play_sound(&name)?;
+                    }
+
+                    log::debug!("Awaiting whereabouts for user {user_id} ...");
+
+                    Ok(Some(user_id))
+                }
+                None => {
+                    log::info!("Unknown user tag: {tag}");
+                    self.play_sound("unknown_user_tag")?;
+
+                    Ok(None)
+                }
+            },
+            Err(e) => {
+                log::warn!("Requesting tag details failed.\n{e}");
+                self.play_sound("oh-nein-netzwerkfehler")?;
+
+                Ok(None)
+            }
+        }
+    }
+
+    pub fn handle_button_press_with_identified_user(
+        &mut self,
+        user_id: UserId,
+        button: Button,
+        party_config: &PartyConfig,
+    ) -> Result<()> {
+        if let Some(whereabouts_name) = &party_config.buttons_to_whereabouts.get(&button) {
+            log::debug!("Submitting whereabouts for user {user_id} -> {whereabouts_name} ...");
+
+            let response = self.update_status(&user_id, whereabouts_name);
+            match response {
+                Ok(_) => {
+                    log::debug!("Status successfully updated.");
+
+                    if let Some(sound_names) =
+                        &party_config.whereabouts_sounds.get(*whereabouts_name)
+                    {
+                        self.play_random_sound(sound_names)?;
+                    }
+                }
+                Err(e) => {
+                    log::warn!("Status update failed.\n{e}");
+                    self.play_sound("oh-nein-netzwerkfehler")?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn update_status(&self, user_id: &UserId, whereabouts_name: &str) -> Result<()> {
+        self.api_client.update_status(user_id, whereabouts_name)
+    }
+
+    fn play_sound(&self, name: &str) -> Result<()> {
+        self.audio_player.play(name)
+    }
+
+    fn play_random_sound(&mut self, names: &[String]) -> Result<()> {
+        let name = self.random.choose_random_element(names);
+        self.play_sound(&name)
+    }
+}
